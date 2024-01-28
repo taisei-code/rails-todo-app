108d7cbe6c848cef4c66723558d0ac5ec8653b9e
Taisei <taisei96.web@gmail.com>
2024-01-29 05:14:16 +0900

    削除機能修正

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,23 @@
 class TodosController < ApplicationController
+
   def index
+    @todos = Todo.all
   end
+
+  def new 
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(title: params[:title])
+    if @todo.save
+      redirect_to action: :index
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'todos#index'
+  # root 'todos#index'
+
+  get    'todos'              => 'todos#index'
+  get    'todos/new'          => 'todos#new'
+  post   'todos/create'       => 'todos#create'
+  delete 'todos/:id'  => "todos#destroy"
+  
+  # resources :todos
+
 end
